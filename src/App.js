2fcf6a342115a4be984b194ec2b5788edbfc92aa
barbara <barbara@bmoah001@gmail.com>
2021-11-08 12:57:38 +0100

    momentarily fixed bug

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ import Typography from "@mui/material/Typography";
 
 function App() {
   const [address, setUserAddress] = React.useState("");
+  const [receiver, setReceiver] = React.useState("");
+  const [amount, setAmount] = React.useState("");
+  const [response, setResponse] = React.useState("");
+
   const [wallet, setWallet] = React.useState("");
   const [usdc, setUsdc] = React.useState("");
 
@@ -67,7 +71,7 @@ function App() {
 
 
 
-async function main() {
+async function getUsdc() {
 
   const usdc = {
     address: "0x68ec573C119826db2eaEA1Efbfc2970cDaC869c4",
@@ -79,20 +83,92 @@ async function main() {
       "function transfer(address _to, uint256 _value) public returns (bool success)",
     ],
   };
+
+
   await provider.send("eth_requestAccounts", []);
   const signer = provider.getSigner();
   
   const usdcContract = new ethers.Contract(usdc.address, usdc.abi, signer);
+  const userAddress = await signer.getAddress();
+   usdcContract.balanceOf(userAddress).then((usdcBalance) => {
+    let newUsdcBalance = ethers.utils.formatUnits(usdcBalance, 6);
+    setUsdc(newUsdcBalance);
+    
+  });
 
-  let usdcBalance = await usdcContract.balanceOf(address);
-  usdcBalance = ethers.utils.formatUnits(usdcBalance, 6);
-  setUsdc(usdcBalance);
+ 
 }
+
+
+
+// async function transferUsdc() {
+ 
+ 
+//   await provider.send("eth_requestAccounts", []);
+//   const signer = provider.getSigner();
+//   let userAddress = await signer.getAddress();
+
+//   const usdcContract = new ethers.Contract(usdc.address, usdc.abi, signer);
+
+//   try {
+//     receiver = ethers.utils.getAddress(receiver);
+//   } catch {
+//     response = `Invalid address: ${receiver}`;
+ 
+//   }
+
+//   try {
+//     amount = ethers.utils.parseUnits(amount, 6);
+//     if (amount.isNegative()) {
+//       throw new Error();
+//     }
+//   } catch {
+//     console.error(`Invalid amount: ${amount}`);
+//     response = `Invalid amount: ${amount}`;
+//     document.getElementById("transferResponse").innerText = response;
+//     document.getElementById("transferResponse").style.display = "block";
+//   }
+
+//   const balance = await usdcContract.balanceOf(userAddress);
+
+//   if (balance.lt(amount)) {
+//     let amountFormatted = ethers.utils.formatUnits(amount, 6);
+//     let balanceFormatted = ethers.utils.formatUnits(balance, 6);
+//     console.error(
+//       `Insufficient balance receiver send ${amountFormatted} (You have ${balanceFormatted})`
+//     );
+
+//     response = `Insufficient balance receiver send ${amountFormatted} (You have ${balanceFormatted})`;
+//     document.getElementById("transferResponse").innerText = response;
+//     document.getElementById("transferResponse").style.display = "block";
+//   }
+//   let amountFormatted = ethers.utils.formatUnits(amount, 6);
+
+
+//   response = `Transferring ${amountFormatted} USDC receiver ${receiver.slice(
+//     0,
+//     6
+//   )}...`;
+//   document.getElementById("transferResponse").innerText = response;
+//   document.getElementById("transferResponse").style.display = "block";
+
+//   const tx = await usdcContract.transfer(receiver, amount, { gasPrice: 20e9 });
+//   document.getElementById(
+//     "transferResponse"
+//   ).innerText += `Transaction hash: ${tx.hash}`;
+
+//   const receipt = await tx.wait();
+//   document.getElementById(
+//     "transferResponse"
+//   ).innerText += `Transaction confirmed in block ${receipt.blockNumber}`;
+// }
+
+
  
 
   useEffect(() => {
     getAddress();
-    main();
+    getUsdc();
   });
 
   // })
